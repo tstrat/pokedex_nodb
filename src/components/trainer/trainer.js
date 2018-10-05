f8351dff071d0292f58373734742f77a602b849f
tstrat <tstrat@email.arizona.edu>
2018-10-05 14:23:26 -0700

    Starting trainer section, added button svg for later

---
+++ src/components/trainer/trainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+
+function Trainer(props) {  
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Trainer;
---
+++ src/components/trainer/trainerList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+
+class TrainerList extends Component {
+constructor() {
+        super()
+        this.state= {};
+    }
+
+
+    render() {
+        return (
+            <div>
+            </div>
+    )}
+}
+
+export default TrainerList;
