88086a7e45d4aef95f5526e08e38cd9106708f2c
tstrat <tstrat@email.arizona.edu>
2018-10-08 11:32:18 -0700

    Add delete functionality

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const controller = require('./controller/controller');
 
 const app = express();
 app.use(bodyParser.json());
+
+const baseURL = '/api/v1';
+
+// All the trainers
+app.get(baseURL + '/trainers', controller.getTrainerList);
+app.post(baseURL + '/trainers', controller.addTrainer);
+
+// Single Trainer
+app.get(baseURL + '/trainers/:id', controller.getTrainer);
+app.put(baseURL + '/trainers/:id', controller.updateTrainer);
+app.patch(baseURL + '/trainers/:id', controller.updateTrainer);
+app.delete(baseURL + '/trainers/:id', controller.removeTrainer);
+
+// Single trainer's pokemon list
+app.post(baseURL + '/trainers/:id/pokemon', controller.addToTrainerPokemonList);
+app.delete(baseURL + '/trainers/:id/pokemon/', controller.removeFromTrainerPokemonList);
 
 const PORT = 4000;
 app.listen(PORT, () =>{
